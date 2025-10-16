4a18d59e7d10092996e22aad73567b764e2f8585
Chiru137 <chiragsarve.01@gmail.com>
2025-10-16 09:25:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Taskform from "./Components/Taskform";
+import TaskList from "./Components/TaskList";
+import Progresstracker from "./Components/Progresstracker";
+
+function App() {
+  return (
+    <div>
+      <h1>Task Focus</h1>
+      <p>Our freindly TaskManager</p>
+      <Taskform/>
+      <TaskList/>
+      <Progresstracker/>
+      <button> Clear all tasks</button>
+    </div>
+  )
+}
+
+export default App;
---
+++ src/Components/TaskList.jsx
@@ -0,0 +1,9 @@
+function TaskList() {
+    return(
+        <div>
+        <h1>TaskList Component</h1>
+        </div>
+    )
+}
+
+export default TaskList;
